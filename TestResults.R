a71c8dc1f2abf2613c482cecc4783600abc50823
“r4dat <rfourtoss@gmail.com>
2015-04-07 20:32:06 -0500

    Add row name expansion and binding. Remove % symbols and cast to numeric.

--- TestResults.R
+++ TestResults.R
@@ -2,5 +2,14 @@
 ## Import results from Google Docs CSV.
 ##
 
-results <- read.csv("C:/Users/USER/Downloads/Results - Sheet1.csv", stringsAsFactors=FALSE)
+library(stringr)
+
+results <- read.csv("C:/Users/rrutherf/Downloads/Results - Sheet1.csv", stringsAsFactors=FALSE)
+
+## Add consistent row-names.
+results=cbind(name=results[rep(seq(1,172,4),each=4),1],results)
+
+## Remove % symbols, cast as numeric.
+results$KNN.Train %<>% str_replace(.,pattern = '%',replacement = '') %>% as.numeric()
+results$KNN.Test %<>% str_replace(.,pattern = '%',replacement = '') %>% as.numeric()
 
