29a6e96d586e9f4c4c776d1306c3c6fefd4c10d4
“r4dat” <rfourtoss@gmail.com>
2015-04-08 09:41:21 -0500

    Add dplyr, skewness lib (e10..), and QQ plot per jhoward@SO.
    Assume result is in local dir.
    
    Add boxplot, numerical summary, histograms, ANOVA, and Kruskal-Wallis.

--- TestResults.R
+++ TestResults.R
@@ -5,8 +5,11 @@
 library(stringr)
 library(magrittr)
 library(ggplot2)
+library(dplyr)
+library(e1071)
+source("ggQQ.R")
 
-results <- read.csv("C:/Users/rrutherf/Downloads/Results - Sheet1.csv", stringsAsFactors=FALSE)
+results <- read.csv("Results - Sheet1.csv", stringsAsFactors=FALSE)
 
 ## Add consistent row-names.
 results=cbind(name=results[rep(seq(1,172,4),each=4),1],results)
@@ -21,10 +24,17 @@ results$KNN.Test %<>% str_replace(.,pattern = '%',replacement = '') %>% as.numer
 ## Rename Series Names to something more informative.
 results[!grepl(pattern = '(Mirroring)|(Haar Wavelet)|(Daubechies Wavelet)',results$Transform),2]="Original"
 
+## Boxplot
+ggplot(results,aes(Transform,KNN.Test))+geom_boxplot()+ylab("Test % Accuracy")
+
+## Show histograms of each Test Set.
+ggplot(results,aes(x=KNN.Test))+geom_histogram()+facet_wrap(~Transform)+ylab("Frequency")+xlab("Test % Accuracy")
+
+## Numerical Summary:
+results %>% select(Transform,KNN.Test) %>% group_by(Transform) %>% summarise_each(funs(mean,sd,skewness)
+
 ## QQ Normality Plots
-qplot(sample=results$KNN.Test,data=results,stat="qq",main = "QQ Plots",xlab = "Theoretical",ylab = "Observations",facets=~Transform)
-
-
+ggQQ(lm(KNN.Test~Transform,data=results))
 
 ## Create the factor structure needed for ANOVA
 results$Transform = as.factor(results$Transform)
@@ -35,4 +45,5 @@ summary(aovmod)
 
 ## Print Type III residual SS
 drop1(aovmod,~.,test="F")
+
 
